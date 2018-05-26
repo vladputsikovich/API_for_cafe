930bad4be55a1d06f4d038fcaa8a08045e9cbad7
vladputsikovich <vladputsikovich@gmail.com>
2018-05-26 17:02:18 +0300

    edit UserController

--- user/UserController.js
+++ user/UserController.js
@@ -4,10 +4,13 @@ var bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
+var firebase = require('firebase');
+var database = firebase.database();
+
 var User = require('./User');
 
 // CREATES A NEW USER
-router.post('/', function (req, res) {
+/*router.post('/', function (req, res) {
     User.create({
             name : req.body.name,
             email : req.body.email,
@@ -17,14 +20,27 @@ router.post('/', function (req, res) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
             res.status(200).send(user);
         });
-});
+});*/
 
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/', function (req, res) {
-    User.find({}, function (err, users) {
+    function getUsers(data){
+        var str = "";
+        for(var x in data){
+             str += ["Login-"+data[x].name , "ID-"+x , "Email-"+data[x].email]+"\n";
+        }
+        if(str == "")   return "Not Found";
+        return str.substring(0, str.length - 1);
+      }
+
+    database.ref('users').once('value',function(snapshot , err) {
+        if (err) return res.status(500).send('Error on the server.');
+        res.status(200).send(getUsers(snapshot.val()));
+    });
+    /*User.find({}, function (err, users) {
         if (err) return res.status(500).send("There was a problem finding the users.");
         res.status(200).send(users);
-    });
+    });*/
 });
 
 // GETS A SINGLE USER FROM THE DATABASE
@@ -38,18 +54,42 @@ router.get('/:id', function (req, res) {
 
 // DELETES A USER FROM THE DATABASE
 router.delete('/:id', function (req, res) {
-    User.findByIdAndRemove(req.params.id, function (err, user) {
+    firebase.database().ref('/users/' + req.body.id)
+        .remove()
+        .then(() => {
+          console.log("then сработал");
+          res.status(200).send("User: "+ user.name +" was deleted.");
+        })
+        .catch(
+          error => {
+            return res.status(500).send("There was a problem deleting the user.");
+          }
+        )
+    /*User.findByIdAndRemove(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem deleting the user.");
         res.status(200).send("User: "+ user.name +" was deleted.");
-    });
+    });*/
 });
 
 // UPDATES A SINGLE USER IN THE DATABASE
 router.put('/:id', function (req, res) {
-    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
+    
+    firebase.database().ref('users/' + req.body.id).set({
+        username: req.body.name,
+        email: req.body.email,
+      }, function(error) {
+        if (error) {
+            return res.status(500).send("There was a problem updating the user.");
+        } else {
+            res.status(200).send("User: "+ user.name +" was updated.");
+        }
+      });
+    
+    
+    /*User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(user);
-    });
+    });*/
 });
 
 
